2847eb201634d5293bd7b2bc2726f112045c7ddb
Ali Zaidan <zaidanal0100@gmail.com>
2026-02-20 22:30:07 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res)=>{
+    res.send("hello backend!")
+})
+app.get('/twitter', (req,res)=>{
+    res.send("alizaidan")
+})
+app.get('/login', (req, res)=>{
+    res.send('<h1>plz login at app</h1>')
+})
+
+app.get('/youtube', (req, res)=>{
+     res.send("<input type='text' placeholder='name'>")
+})
+app.listen(process.env.PORT, ()=>{
+    console.log(`Example app listening on port ${port}`)
+})
